f2591c73d19152b1d1f3640eaf556734c003919c
Caroline Chen <caroline.x.chen@duke.edu>
2023-04-30 18:40:08 -0400

    basic working website

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # 721-final
+
+## Overview
+
+## 
+References:
+1. Guide for [pipenv](https://towardsdatascience.com/comparing-python-virtual-environment-tools-9a6543643a44)
--- app.py
+++ app.py
@@ -1,20 +1,33 @@
-from flask import Flask
-from flask_restplus import Api, Resource
+from flask import Flask, render_template, request, send_file
+import requests
+import io
+from PIL import Image
 
-flask_app = Flask(__name__)
-app = Api(app=flask_app)
+app = Flask(__name__)
 
-name_space = app.namespace('main', description='Main APIs')
+API_URL = "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5"
+# Replace with your actual API token
+API_TOKEN = "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"
+headers = {"Authorization": f"Bearer {API_TOKEN}"}
 
 
-@name_space.route("/")
-class MainClass(Resource):
-    def get(self):
-        return {
-            "status": "Data retrieved successfully."
-        }
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    print(response.content)
+    print(response.headers['Content-Type'])
+    return response.content
 
-    def post(self):
-        return {
-            "status": "Data posted successfully."
-        }
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        text_input = request.form['text_input']
+        image_bytes = query({"inputs": text_input})
+        image = Image.open(io.BytesIO(image_bytes))
+        image.save('static/generated_image.jpg', 'JPEG')
+        return send_file('static/generated_image.jpg', mimetype='image/jpeg')
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
