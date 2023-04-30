cb523c699de8d46729d4f3c548e8a753c25687d8
Andy <andy.kao@duke.edu>
2023-04-30 19:56:29 -0400

    from Andy

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
-from flask import Flask
-from flask_restplus import Api, Resource
+from flask import Flask, render_template, request, send_file
+import requests
+import io
+from PIL import Image
 
-flask_app = Flask(__name__)
-app = Api(app=flask_app)
+app = Flask(__name__)
 
-name_space = app.namespace('main', description='Main APIs')
+API_URL = "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5"
+headers = {"Authorization": f"Bearer {'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'}"}
 
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        prompt = request.form['prompt']
+        payload = {"inputs": prompt}
+        response = requests.post(API_URL, headers=headers, json=payload)
+        image_bytes = response.content
+        image = Image.open(io.BytesIO(image_bytes))
+        img_io = io.BytesIO()
+        image.save(img_io, 'PNG')
+        img_io.seek(0)
+        return send_file(img_io, mimetype='image/png')
+    return render_template('index.html')
 
-@name_space.route("/")
-class MainClass(Resource):
-    def get(self):
-        return {
-            "status": "Data retrieved successfully."
-        }
-
-    def post(self):
-        return {
-            "status": "Data posted successfully."
-        }
+if __name__ == '__main__':
+    app.run(debug=True)
